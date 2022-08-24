a6a1a5d119d5051c2c2273c7933a6656ac9b4c7b
Daniil-Tevs <daniil_tevs@mail.ru>
2022-08-24 20:36:17 +0200

    Refactor button

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def start_dialog(message):
                      reply_markup=markup)
 
 
+def make_act(message):
+    pass
+
+
 def find_material(message):
     bot.send_message(message.chat.id, message.text)
     # запрос
@@ -51,44 +55,49 @@ def report_handler(message):
 
 
 @bot.message_handler(content_types=["text"])
-def bar(message):
+def work(message):
     if message.text == "Организация":
-        type = "organization"
         organization_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        find_button = telebot.types.KeyboardButton("Проверка материала на наличие в реестре запрещённых материалов")
-        check_button = telebot.types.KeyboardButton("Проверка списка материалов в реестре на наличие запрещённой литературы")
-        make_act_button = telebot.types.KeyboardButton("Составление акта")
+        find_button = telebot.types.KeyboardButton("1)Проверка материала на наличие в реестре запрещённых материалов")
+        check_button = telebot.types.KeyboardButton(
+            "2)Проверка списка материалов в реестре на наличие запрещённой литературы")
+        make_act_button = telebot.types.KeyboardButton("3)Составление акта")
 
         organization_markup.add(find_button, check_button, make_act_button)
         bot.send_message(message.chat.id,
-                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find [name]\n2)"
+                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find ["
+                         "name]\n2) "
                          "Проверка списка материалов в реестре на наличие запрещённой литературы - /check"
                          "\n3)Составление акта",
                          reply_markup=organization_markup)
     elif message.text == "Личное пользование":
-        type = "person"
         person_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
         find_button = telebot.types.KeyboardButton("Проверка материала на наличие в реестре запрещённых материалов")
         report_button = telebot.types.KeyboardButton("Отправить жалобу на материал")
         person_markup.add(find_button, report_button)
         bot.send_message(message.chat.id,
-                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find [name]\n2)Отправить жалобу на материал - /report [name]",
+                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find ["
+                         "name]\n2)Отправить жалобу на материал - /report [name]",
                          reply_markup=person_markup)
-    elif message.text == "Проверка материала на наличие в реестре запрещённых материалов":
+    elif message.text == "1)Проверка материала на наличие в реестре запрещённых материалов":
         bot.send_message(message.chat.id, "Отправьте название")
         bot.register_next_step_handler(message, find_material)
-    elif message.text == "Проверка списка материалов в реестре на наличие запрещённой литературы":
+    elif message.text == "2)Проверка списка материалов в реестре на наличие запрещённой литературы":
         bot.send_message(message.chat.id, "Отправьте название")
         bot.register_next_step_handler(message, check_material)
-    elif message.text == "Отправить жалобу на материал":
-        bot.send_message(message.chat.id, "Отправьте название")
-        bot.register_next_step_handler(message, report_material)
+    elif message.text == "3)Составление акта":
+
+        bot.register_next_step_handler(message, make_act)
     elif message.text == "2)Отправить жалобу на материал":
-        bot.send_message(message.chat.id, "Опишите свою проблему")
-        #берёт описание проблемы
+        bot.send_message(message.chat.id, "пишите вашу проблему")
+        # берёт описание проблемы
         bot.register_next_step_handler(message, report_material)
     else:
-        bot.send_message(message.chat.id,"Непонятная команда /help")
+        bot.send_message(message.chat.id, "Непонятная команда /help")
 
-bot.polling(none_stop=True)
 
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception:
+        pass

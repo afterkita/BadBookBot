f32b24ee1eee071be9fee7b1adfb1898bcb5a2c2
Daniil-Tevs <daniil_tevs@mail.ru>
2022-08-25 14:03:17 +0200

    refactor messages

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ BAD_BOOKS = get_books()
 MARCUP = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 find_button = telebot.types.KeyboardButton("Поиск")
 check_button = telebot.types.KeyboardButton("Анализ")
-report_button = telebot.types.KeyboardButton("Жалоба")
+report_button = telebot.types.KeyboardButton("Обращение")
 info_button = telebot.types.KeyboardButton("Инфо")
 education_button = telebot.types.KeyboardButton("Просвещение")
-MARCUP.add(find_button, check_button, report_button, info_button)
+MARCUP.add(find_button, check_button, report_button, info_button, education_button)
 
 EmptyMarcup = telebot.types.ReplyKeyboardMarkup()
 
@@ -36,10 +36,10 @@ def start_schedule(id):
         if status:
             for i in get_users():
                 bot.send_message(i, "Реестр обновился, проведите анализ библиотеки!")
-        time.sleep(60 * 60 * 24 * 7)
+        time.sleep(60 * 60 * 24)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'restart'])
 def start_dialog(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     organization_button = telebot.types.KeyboardButton('Организация')
@@ -49,23 +49,27 @@ def start_dialog(message):
     make_user(message.chat.id, message.from_user.username)
 
     bot.send_message(message.chat.id,
-                     'Привет, я телеграм-бот BadBookBot, который проверяет различные материалы на наличие их в регистре экстремистских материалов МЮ РФ',
+                     'Привет, я телеграм-бот BadBookBot, который проверяет различные материалы на наличие их в Федеральном реестре экстремистских материалов',
                      reply_markup=markup)
     bot.send_message(message.chat.id,
-                     'Если вы собираетесь использовать для отслеживания запрещённых книг в своей библиотеке, то нажмите Организация, иначе нажмите Личное пользование ',
+                     'Для мониторинга наличия экстремистских книг в фонде библиотеки - нажмите Организация.\nДля '
+                     'поиска информации в реестре в личных или коммерческих целях - нажмите Личное пользование',
                      reply_markup=markup)
 
 
 def act_choose(message):
     if message.text.split()[0].lower() == "нет":
         users[message.chat.id] = [config.act_dialog(), []]
-        bot.register_next_step_handler(bot.send_message(message.chat.id, "Пришлите мне файл с вашей библиотекой"),
-                                       get_doc)
+        bot.register_next_step_handler(bot.send_message(message.chat.id,
+                                                        "Пришлите текстовый файл с перечнем литературы, содержащейся в фонде вашей библиотеке",
+                                                        reply_markup=telebot.types.ReplyKeyboardRemove()), get_doc)
     elif message.text.split()[0].lower() == "да":
         users[message.chat.id][1].append(message.chat.id)
         src = make_template(*users[message.chat.id][1])
 
         with open(src, 'rb') as f:
+            bot.send_message(message.chat.id,
+                             "Для вас подготовлен акт о наличии в фонде библиотеки экстремистских материалов. Поместите указанный документ в отдельное помещение или шкаф с замком, специально выделенное помещение для хранения подобных документов.")
             bot.send_document(message.chat.id, document=f, reply_markup=MARCUP)
             f.close()
             os.remove(src)
@@ -90,7 +94,7 @@ def make_act(message):
             users[message.chat.id][1][1], users[message.chat.id][1][2], users[message.chat.id][1][3],
             users[message.chat.id][1][4]))
 
-        bot.send_message(message.chat.id, "Все правильно?")
+        bot.send_message(message.chat.id, "Все правильно?", reply_markup=markup)
         bot.register_next_step_handler(message, act_choose)
 
     else:
@@ -98,7 +102,46 @@ def make_act(message):
 
 
 def get_education_information(message):
-    pass
+    bot.send_message(message.chat.id, "За хранение экстремистских материалов в целях массового распространения "
+                                      "существует административная ответственность, для граждан штраф от 1 до 3 тысяч "
+                                      "рублей или арест на срок до 15 суток. Для юридических лиц штраф от ста тысяч до "
+                                      "одного миллиона рублей или приостановление деятельности на срок до девяноста "
+                                      "суток (ст. 20.29 КоАП РФ).\nПод экстремистскими материалами понимаются "
+                                      "предназначенные для обнародования\n- документы либо информация на иных "
+                                      "носителях, призывающие к осуществлению экстремистской деятельности либо "
+                                      "обосновывающие или оправдывающие необходимость осуществления такой "
+                                      "деятельност\n- труды руководителей национал-социалистической рабочей партии "
+                                      "Германии, фашистской партии Италии,\n- публикации, обосновывающие либо "
+                                      "оправдывающие национальное и (или) расовое превосходство либо оправдывающие "
+                                      "практику совершения военных или иных преступлений, направленных на полное или "
+                                      "частичное уничтожение какой-либо этнической, социальной, расовой, национальной "
+                                      "или религиозной группы\nЛитературу признают экстремистским материалом "
+                                      "федеральным судом по месту их обнаружения, распространения или нахождения "
+                                      "организации, осуществившей производство таких материалов, на основании "
+                                      "заявления прокурора или при производстве по соответствующему делу об "
+                                      "административном правонарушении, гражданскому или уголовному делу. Одновременно "
+                                      "с решением о признании информационных материалов экстремистскими судом "
+                                      "принимается решение об их конфискации. Копия вступившего в законную силу "
+                                      "решения о признании информационных материалов экстремистскими направляется "
+                                      "судом в трехдневный срок в Минюст. Минюст добавляет информацию в Федеральный "
+                                      "список экстремистских материалов\nПо данным Судебного Департамента при "
+                                      "Верховном Суде Российской Федерации судами общей юрисдикции за 2021 год "
+                                      "рассмотрено 1519 дел по ст. 20.29 КоАП РФ «Производство и распространение "
+                                      "экстремистских материалов», назначено наказание 1319 лицам (6 юридическим "
+                                      "лицам, 15 должностным лицам, 1 292 физическим лицам)\n")
+    bot.send_message(message.chat.id, "Если Вы обнаружили в "
+                                      "библиотечном фонде экстремистский материал, то Вам необходимо:\n1. Маркировать "
+                                      "издание. Наклейте на обложку этикетку с восклицательным знаком или любой другой "
+                                      "символикой, которая будет указывать на запрет распространения данного "
+                                      "документа. Важно, чтобы эту маркировку знали и понимали все сотрудники "
+                                      "библиотеки.\n2. Составить Акт о наличии в библиотеке издания, включенного в "
+                                      "федеральный список экстремистских материалов.\n3. Выделить для хранения "
+                                      "специальное место. Это может быть отдельное помещение, закрытый стеллаж, "
+                                      "шкаф с замком, где будут размещаться документы, включенные в Список.\n4. "
+                                      "Назначить ответственного за хранение и движение этих документов приказом по "
+                                      "библиотеке.\n5. Запретить доступ к фонду экстремистских материалов всем, "
+                                      "кто в этом приказе не упомянут.\n6. Составить опись документов, хранящихся в "
+                                      "выделенном фонде, и проверять, на месте ли они, один раз в год.")
 
 
 def get_doc(message):
@@ -115,13 +158,18 @@ def get_doc(message):
         bad_books_in_my_collection = list(BAD_BOOKS & my_books)
 
         if bad_books_in_my_collection:
-            bot.send_message(message.chat.id, "Найдены совпадения. Начинаю составлять акт.")
+            bot.send_message(message.chat.id, "Найдены совпадения. Начинаю составлять акт о наличии в библиотеке "
+                                              "издания, включенного в ФСЭМ, в соответствии с пунктом 3.1 Рекомендаций "
+                                              "по работе библиотек с документами \"УТВ. МИНКУЛЬТ РФ от 12.09.2017 "
+                                              "г.\" Мне необходимо будет знать ФИО вашего руководителя и трёх сотрудников, включая вас, которые будут подписывать акт")
             users[message.chat.id][1].append(bad_books_in_my_collection)
 
             bot.register_next_step_handler(
                 bot.send_message(message.chat.id, next(users[message.chat.id][0])), make_act)
         else:
-            bot.send_message(message.chat.id, "Совпадения не обнаружены!", reply_markup=MARCUP)
+            bot.send_message(message.chat.id,
+                             "Совпадения не обнаружены. В вашем фонде отсутствуют экстремистские материалы. Не забудьте провести новую проверку после получения уведомления о включении новых книг в реестр",
+                             reply_markup=MARCUP)
     except Exception:
         bot.send_message(message.from_user.id, "Не получилось открыть данный файл", reply_markup=MARCUP)
 
@@ -131,12 +179,14 @@ def find_material(message):
     if result:
         bot.send_message(message.chat.id, result, reply_markup=MARCUP)
     else:
-        bot.send_message(message.chat.id, "Отсутствует", reply_markup=MARCUP)
+        bot.send_message(message.chat.id,
+                         "В Федеральном реестре экстремистских материалов информация об этом источнике отсутствует",
+                         reply_markup=MARCUP)
 
 
 def report_choose(message):
     if message.text.split()[0].lower() == "нет":
-        bot.send_message(message.chat.id, "Начнём заново")
+        bot.send_message(message.chat.id, "Начнём заново", reply_markup=telebot.types.ReplyKeyboardRemove())
         users[message.chat.id] = [config.act_dialog_report(), []]
         bot.register_next_step_handler(bot.send_message(message.chat.id, next(users[message.chat.id][0])), make_report)
     elif message.text.split()[0].lower() == "да":
@@ -144,8 +194,9 @@ def report_choose(message):
         src = make_template_report(*users[message.chat.id][1])
 
         with open(src, 'rb') as f:
-            bot.send_document(message.chat.id, document=f)
-            bot.send_message(message.chat.id, "", reply_markup=MARCUP)
+            bot.send_message(message.chat.id,
+                             "Пожалуйста, распечатайте прикреплённый файл, в шапке обращения укажите ФИО прокурора города и не забудьте поставить свою подпись")
+            bot.send_document(message.chat.id, document=f, reply_markup=MARCUP)
             f.close()
             os.remove(src)
         return
@@ -157,7 +208,7 @@ def make_report(message):
     w = next(users[message.chat.id][0])
     users[message.chat.id][1].append(message.text)
     if w == '':
-        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
         yes_button = telebot.types.KeyboardButton("Да")
         no_button = telebot.types.KeyboardButton("Нет")
         markup.add(yes_button, no_button)
@@ -193,7 +244,8 @@ def check_handler(message):
     users[message.chat.id] = [config.act_dialog(), []]
     # проведена проверка и вызов функции формировании акта
 
-    bot.register_next_step_handler(bot.send_message(message.chat.id, "Пришлите файл с вашей библиотекой",
+    bot.register_next_step_handler(bot.send_message(message.chat.id,
+                                                    "Пришлите текстовый файл с перечнем литературы, содержащейся в фонде вашей библиотеке",
                                                     reply_markup=telebot.types.ReplyKeyboardRemove()), get_doc)
 
 
@@ -209,47 +261,49 @@ def information_bot(message):
 
 
 @bot.message_handler(commands=['education'])
-def education():
-    pass
+def education(message):
+    bot.register_next_step_handler(message, get_education_information)
 
 
 @bot.message_handler(commands=['help'])
 def help_handler(message):
     bot.send_message(message.chat.id,
-                     "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find ["
-                     "name]\n2) "
-                     "Проверка списка материалов в реестре на наличие запрещённой литературы - /check"
+                     "Список команд:\n1)Получить информацию о боте - /info Проверка материала на наличие в реестре запрещённых материалов - /find [name]"
+                     "\n2)Проверка списка материалов в реестре на наличие запрещённой литературы - /check"
                      "\n3) Отправить жалобу на материал - /report "
-                     "\n4) Получить информацию о боте - /info ", reply_markup=MARCUP)
+                     "\n4) ", reply_markup=MARCUP)
 
 
 @bot.message_handler(content_types=["text"])
 def work(message):
     if message.text == "Организация":
         bot.send_message(message.chat.id,
-                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find ["
+                         "Список команд:\n1) Проверка конкретной книги на наличие её в Федеральном реестре экстремистских материалов - /find ["
                          "name]\n2) "
-                         "Проверка списка материалов в реестре на наличие запрещённой литературы - /check",
+                         "Выявление экстремистских материалов в фонде библиотеки и составление акта об обнаружении - /check",
                          reply_markup=MARCUP)
     elif message.text == "Личное пользование":
         bot.send_message(message.chat.id,
-                         "Список команд:\n1)Проверка материала на наличие в реестре запрещённых материалов - /find ["
-                         "name]\n2)Отправить жалобу на материал - /report",
+                         "Список команд:\n1) Проверка конкретной книги на наличие её в Федеральном реестре экстремистских материалов - /find ["
+                         "name]\n2) Составление обращения в органы прокуратуры о проверке книги на наличие экстремистского материала - /report",
                          reply_markup=MARCUP)
     elif message.text == "Поиск":
         bot.send_message(message.chat.id, "Отправьте название", reply_markup=telebot.types.ReplyKeyboardRemove())
         bot.register_next_step_handler(message, find_material)
     elif message.text == "Анализ":
         check_handler(message)
-    elif message.text == "Жалоба":
+    elif message.text == "Обращение":
+        bot.send_message(message.chat.id, "Начинаю составлять обращение. Мне нужно немного больше информации")
         users[message.chat.id] = [config.act_dialog_report(), []]
         bot.register_next_step_handler(bot.send_message(message.chat.id, next(users[message.chat.id][0]),
                                                         reply_markup=telebot.types.ReplyKeyboardRemove()), make_report)
     elif message.text == "Информация":
         get_education_information(message)
+    elif message.text == "Просвещение":
+        get_education_information(message)
     else:
         bot.send_message(message.chat.id, "Непонятная команда. Используйте /help",
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
+                         reply_markup=MARCUP)
 
 
 if __name__ == '__main__':

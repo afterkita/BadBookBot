d5f1cb113f6b31f14880f7825fdfea389fbe65d9
udogaL2 <57914563+udogaL2@users.noreply.github.com>
2022-08-25 02:49:26 +0200

    update

--- create_report.py
+++ create_report.py
@@ -2,11 +2,11 @@ from docxtpl import DocxTemplate
 from datetime import date
 
 
-def make_template_report(main_name, adress, number, mail, name_material, author_material,id):
+def make_template_report(main_name, adress, number, mail, name_material, author_material, id):
     doc = DocxTemplate("report.docx")
-    context = {'ФИО': main_name, 'address': adress, 'number': number,'mail':mail,'НАЗВАНИЕ':"\"{}\"".format(name_material),'ФИО_автор':author_material,'date':date.today()}
+    context = {'ФИО': main_name, 'address': adress, 'number': number, 'mail': mail,
+               'НАЗВАНИЕ': "\"{}\"".format(name_material), 'ФИО_автор': author_material, 'date': date.today()}
     doc.render(context)
     doc.save(f"report-result{id}.docx")
 
     return f"report-result{id}.docx"
-
